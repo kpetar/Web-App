f982d516ec29dea60c02130e746c437edccd4fb3
Petar Kovac <petark218@gmail.com>
2021-03-28 18:31:34 +0200

    Updated security

--- config/database.configuration.ts
+++
@@ -1,7 +0,0 @@
-//parametri neophodni za vezu sa bazom podataka, ekspotruje jedan objekat
-export const DatabaseConfiguration={
-    hostname:'localhost',
-    username:'aplikacija',
-    password:'aplikacija',
-    database:'aplikacija'
-};
